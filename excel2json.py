f6256dc9d2192eff8cd670b89e8cc942728fa22f
Isabel <isabel@hermit.no>
2022-03-22 14:05:13 +0100

    converter programs

--- excel2json.py
+++ excel2json.py
@@ -1,8 +1,8 @@
 from csv import excel
 import pandas as pd
 
-def excel2json(xlsxfilename: str, jsonfilename: str):
+def excel2json(xlsxfilename: str):
     data = pd.read_excel(xlsxfilename, header=2)
-    data.to_json(jsonfilename, orient='table')
+    json_str = data.to_json(path=None, orient='table')
+    return json_str
 
-    
