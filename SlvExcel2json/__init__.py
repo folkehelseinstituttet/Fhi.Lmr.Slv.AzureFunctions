75899ce11144ec1ecfda28741a853e63b58f0bf9
Terje Sandstrom <terje@hermit.no>
2022-03-28 16:12:16 +0200

    updated for azure

--- SlvExcel2json/__init__.py
+++ SlvExcel2json/__init__.py
@@ -1,26 +1,34 @@
 import logging
 
 import azure.functions as func
-from csv import excel
 import pandas as pd
-from excel2json import excel2json
+# from urllib.parse import quote
+
+def excel2json(xlsxfilename: str):
+    '''Function to convert an excel file to json.
+    the argument xlsxfilename takes either a file, path or url.
+    Returns a string with the json text'''
+    logging.info('Inside excel2json')
+    data = pd.read_excel(xlsxfilename, header=2)
+    logging.info('Excel read')
+    data = data.fillna("")
+    json_str = data.to_json(orient='records')
+    logging.info('Converted to json')
+    return json_str
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('SLV-Excel2json Python HTTP trigger function processed a request.')
-
-    url = req.params.get('url')
-    if not url:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            url = req_body.get('url')
-
-    if url:
-        return func.HttpResponse(excel2json(url))
+    
+    urlraw = req.params.get('url')
+    logging.info(urlraw)
+    # url='https://legemiddelverket.no/Documents/Import%20og%20salg/Apotekdrift/Apotektillatelser/landets%20apotek%20.xlsx'
+    if urlraw:    
+        url = urlraw.replace(' ','%20')
+        logging.info('Parameter is '+url)
+        json = excel2json(url)
+        return func.HttpResponse(json)
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a url to an Apotek xlsx in the query string or in the request body to convert to json.",
+             "This HTTP triggered function executed successfully. Pass a url parameter to an Apotek xlsx in the query string to convert to json.",
              status_code=200
         )
